5918a7237777a7fb06b571e7932b1b404ee33c7b
Pastel Turtle <60445211+beckyeng226@users.noreply.github.com>
2021-03-18 14:59:00 -0400

    initial commit

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {TrackList} from './TrackList';
+import './SearchResults.css';
+
+export class SearchResults extends React.Component {
+    render() {
+        <div className="SearchResults">
+  <h2>Results</h2>
+  {/* <TrackList /> */}
+</div>
+    }
+}
